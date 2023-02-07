dcfd715627d3b34935ad4affc2f7e7141dbb59b6
juandre1298 <juandre1298@gmail.com>
2023-02-07 11:03:21 -0500

    module 11, how to make a form, its usefull to make account registration

---
+++ react-code/module11-froms-in-react/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Form } from "./components/Form";
+// to make forms we need to install react-hook-form
+// npm install react-hook-form
+// also need to: npm install  react-hook-from yup
+function App() {
+  return (
+    <div className="app">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
