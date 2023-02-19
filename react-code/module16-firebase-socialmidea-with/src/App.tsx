b3436414af8818bb979165a5fec2538159c360c2
juandre1298 <juandre1298@gmail.com>
2023-02-19 17:35:49 -0500

    social media project

---
+++ react-code/module16-firebase-socialmidea-with/src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+// fist we will need to install: npm install react-router-dom
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Main } from "./pages/main";
+import { Login } from "./pages/login";
+import { Navbar } from "./components/navbar";
+import { CreatePost } from "./pages/create-post/create-post";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
